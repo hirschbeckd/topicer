48be6e5b17db4d7bd534eaacf8abae6fc00cb1fa
Daniel.Hirschbeck <daniel.hirschbeck@idomsoft.hu>
2020-08-28 08:36:04 +0200

    application skeleton

---
+++ src/main/kotlin/hu/hirschbeckd/topicer/TopicerApplication.kt
@@ -0,0 +1,11 @@
+package hu.hirschbeckd.topicer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TopicerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TopicerApplication>(*args)
+}
---
+++ src/test/kotlin/hu/hirschbeckd/topicer/TopicerApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.hirschbeckd.topicer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TopicerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
